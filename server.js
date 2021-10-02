5399d88fec58e9098472a2c12d5407dd24d5fa5a
ya1k96 <yamil123_1@live.com>
2021-10-02 10:40:47 -0300

    update server

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 
 //Declaracion de rutas
-const usersRoute = require('./api/users');
 const characterRoute = require('./api/character');
 const movieRoute = require('./api/movie');
 const authRoute = require('./api/auth');
 
 app.use(express.json());
 
-app.use('/api/users', usersRoute);
 app.use('/api/characters', characterRoute);
 app.use('/api/movies', movieRoute);
 app.use('/api/auth', authRoute);
